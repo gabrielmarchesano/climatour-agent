1cc6f8193b4245c26bbfcdfe7495f8ec742d2d9d
gabrielmarcesano <gabrielmarchesano.contato@gmail.com>
2026-09-13 16:10:49 -0300

    fix incorrect param

--- tools/agent.py
+++ tools/agent.py
@@ -1,19 +1,25 @@
 from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
 from tools.tools import get_clima
 from dotenv import load_dotenv
 
 load_dotenv()
+
+model = init_chat_model(
+    "groq:openai/gpt-oss-120b",
+    temperature=0.1,
+)
+
 agent = create_agent(
-    model="groq:openai/gpt-oss-120b",
+    model=model,
     tools=[get_clima],
-    temperature=0,
     system_prompt=(
         "Você é um agente de turismo. Dado um estado brasileiro, "
         "sugira cidades candidatas com atrações turísticas, use a "
         "ferramenta get_clima para checar o clima de cada "
         "uma. explique o porquê de cada recomendação citando o dado de clima (temperatura X, condição Y → passeio Z)." \
         "Por favor, forneça a resposta em português e não inclua informações de clima para cidades fora do estado informado."
-        "A saída da resposta deve ser sempre: Cidade → Clima atual → 3 passeios → justificativa"
+        "A saída da resposta deve ser sempre: Cidade → Clima atual → 3 passeios → justificativa. Responda apenas isso e mais nada"
         "Pense que climas extremos (muito frio, muito calor, chuva) podem impactar negativamente a experiência do passeio e que certos passeios ficam impedidos de serem realizados em determinadas condições climáticas. Portanto, considere o clima ao sugerir passeios e explique como o clima influencia a experiência do passeio."
     ),
 )
